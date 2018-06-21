448a689af71f2ee7dd7694fbc0362c155c4ddf3b
Andy Stewart <boss@airbladesoftware.com>
2018-06-21 11:22:41 +0100

    Use actual filenames.

--- install.sh
+++ install.sh
@@ -1,42 +1,56 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Installs the dotfiles into the user's home directory.
 #
 # NOTE: the bash dotfiles assume a layout of ~/dotfiles
 
-set -e
+set -eo pipefail
 
 # Directory in which this script is located.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for file in *; do
-  # TODO: skip this install.sh file too
-  if [ $file = "README.md" ]; then
-    continue
-  fi
+# Loop over files and directories in dotfiles directory.
+#
+# This approach handles spaces etc in filenames (which shouldn't really be necessary here).
+#
+# Note in order to read input from user inside the loop, we redirect stdin to file
+# descriptor 3.  Otherwise the input would be consumed from the list of filenames.
+while IFS=  read -r -d $'\0'; do
 
-  sourcefile="${DIR}/${file}"
-  dotfile="$HOME/.${file}"
+  source="$REPLY"
+  filename="${source##*/}"
 
-  if [ -e "${dotfile}" ]; then
-    if [ $replace_all ]; then
-      ln -fs "${sourcefile}" "${dotfile}"
+  if [[ $filename == "dotfiles" ]]; then continue; fi
+
+  target="$HOME/$filename"
+
+  # We could use `ln -i` to prompt user if the target file exists
+  # but it doesn't take account of the target being what we want anyway.
+
+  if [ -e "$target" ]; then
+    if [ "$(readlink "$target")" = "$source" ]; then
+      echo skip: "$source" → "$target"
     else
-      echo -n "replace ${dotfile}? [ynaq] "
-      read keypress
-      case "$keypress" in
-        "y" ) ln -nfs "${sourcefile}" "${dotfile}" ;;
-        "n" ) continue ;;
-        "a" )
-          replace_all="yes"
-          ln -nfs "${file}" "${dotfile}"
-        ;;
-        "q" ) exit 0 ;;
+      read -u 3 -r -n 1 -p "overwrite $target (y/n)? " answer
+      case "${answer:0:1}" in
+        y|Y)
+          ln -Ffs "$source" "$target" && echo link: "$source" → "$target"
+          ;;
+        *)
+          echo skip: "$source" → "$target"
+          ;;
       esac
     fi
   else
-    ln -s "${sourcefile}" "${dotfile}"
+    ln -s "$source" "$target" && echo link: "$source" → "$target"
   fi
-done
 
-echo "Installed dotfiles in $HOME"
+done 3<&0 < <(find "$DIR" -maxdepth 1 \
+         -not -name .DS_Store    \
+         -not -name .git         \
+         -not -name bash         \
+         -not -name README.md    \
+         -not -name install.sh   \
+         -print0                 \
+        )
+
